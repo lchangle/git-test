2b8d9162cdc06fec4a8b37b4da30d5d536fa9f02
lchangle <60434482+lchangle@users.noreply.github.com>
2022-05-11 17:19:30 +0800

    pull commit

--- src/main/java/com/lyh/git/GitTest.java
+++ src/main/java/com/lyh/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git master!");
         System.out.println("hello git hot-fix2!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
